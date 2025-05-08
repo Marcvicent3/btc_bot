253ec4b6f1e960dca213e0b186f9f730c4bf57ea
Marcvicent3 <marcelovromeroa@hotmail.com>
2025-05-07 22:01:54 -0500

    chore(.gitignore): update .gitignore to exclude subscribers.txt and last_buy_*.txt files feat(main.py): add a simple web server using Flask to display a message when accessing the root route feat(main.py): launch Flask in a separate thread to run concurrently with the bot feat(requirements.txt): add necessary Python packages for the project including Flask, pandas, numpy, and others

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 from bot import app
+from flask import Flask
+import threading
+
+# Crea un pequeño servidor web
+web_app = Flask('')
+
+@web_app.route('/')
+def home():
+    return "Bot de BTC activo ✅"
+
+def run_web():
+    web_app.run(host='0.0.0.0', port=3000)
+
+# Lanza Flask en un hilo aparte
+threading.Thread(target=run_web).start()
 
 if __name__ == "__main__":
     # Arranca el bot (polling) y mantiene el loop abierto
